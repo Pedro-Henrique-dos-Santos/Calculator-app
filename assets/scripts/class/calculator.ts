98b999804bacc4390c8c4902280928064dab9267
Leeondras <115871293+Pedro-Henrique-dos-Santos@users.noreply.github.com>
2023-01-22 11:37:28 -0300

    second commit

--- assets/scripts/class/calculator.ts
+++ assets/scripts/class/calculator.ts
@@ -1,118 +1,116 @@
-import { Screnn } from "./screen.js"
+import { Screnn } from "./screen.js";
 
 interface Calculator {
-    labels: any
-    dataTheme: any
-    mathPanel: any
-    numbers: any
-    operators: any
-    resources: any
-    result: any
+  labels: NodeList;
+  dataTheme: NodeList;
+  mathPanel: Element;
+  numbers: NodeList;
+  operators: NodeList;
+  resources: NodeList;
+  result: Element;
 }
 
 class Calculator extends Screnn {
+  functionsCalculator(): void {
+    this.mathPanel = document.querySelector(".math-panel");
+    this.numbers = document.querySelectorAll(".number");
+    this.operators = document.querySelectorAll(".operator");
+    this.resources = document.querySelectorAll(".resources");
+    this.result = document.querySelector(".result");
 
-    functionsCalculator(): void {
-        this.mathPanel = document.querySelector('.math-panel')
-        this.numbers = document.querySelectorAll('.number')
-        this.operators = document.querySelectorAll('.operator')
-        this.resources = document.querySelectorAll('.resources')
-        this.result = document.querySelector('.result')
-        this.numbers.forEach(number => {
-            number.addEventListener('click', () => {
-                this.mathPanel.innerHTML += number.innerText
-            })
-        })
-        this.operators.forEach(operator => {
-            operator.addEventListener('click', () => {
-                if(this.mathPanel.innerHTML.includes('+') || this.mathPanel.innerHTML.includes('-') || this.mathPanel.innerHTML.includes('x') || this.mathPanel.innerHTML.includes('/')){
-                    return
-                }
-                this.mathPanel.innerHTML += operator.innerText
-            })
-        })
-        this.resources.forEach(data => {
-            data.addEventListener('click', () => {
-                if (data.innerHTML === 'reset') {
-                    this.mathPanel.innerHTML = ''
+    this.numbers.forEach((number:Element) => {
+      number.addEventListener("click", () => {
+        this.mathPanel.textContent += number.textContent;
+      });
+    });
+    this.operators.forEach((operator:Element) => {
+      operator.addEventListener("click", () => {
+        if (
+          this.mathPanel.innerHTML.includes("+") ||
+          this.mathPanel.innerHTML.includes("-") ||
+          this.mathPanel.innerHTML.includes("x") ||
+          this.mathPanel.innerHTML.includes("/")
+        ) {
+          return;
+        }
+        this.mathPanel.textContent += operator.textContent;
+      });
+    });
+    this.resources.forEach((data:Element) => {
+      data.addEventListener("click", () => {
+        if (data.textContent === "reset") {
+          this.mathPanel.innerHTML = "";
+        }
+        if (data.innerHTML === "del") {
+          const att: String = this.mathPanel.innerHTML;
+          this.mathPanel.textContent = att.slice(0, -1);
+        }
+      });
+    });
 
-                }
-                if (data.innerHTML === 'del') {
-                    const att: any = this.mathPanel.innerHTML
-                    this.mathPanel.innerText = att.slice(0, -1)
+    this.result.addEventListener("click", () => {
+      if (this.mathPanel.innerHTML.includes("+")) {
+        let panel: String = this.mathPanel.innerHTML;
+        const previus = panel.split("+");
+        const resultOperation: Number =
+          parseInt(previus[0]) + parseInt(previus[1]);
+        this.mathPanel.textContent = resultOperation.toString();
+      }
 
-                }
-            })
+      if (this.mathPanel.innerHTML.includes("-")) {
+        let panel: String = this.mathPanel.innerHTML;
+        const previus = panel.split("-");
+        const resultOperation: Number =
+          parseInt(previus[0]) - parseInt(previus[1]);
+        this.mathPanel.textContent = resultOperation.toString();
+      }
 
-        })
+      if (this.mathPanel.innerHTML.includes("/")) {
+        let panel: String = this.mathPanel.innerHTML;
+        const previus = panel.split("/");
+        const resultOperation: Number =
+          parseInt(previus[0]) / parseInt(previus[1]);
+        this.mathPanel.textContent = resultOperation.toString();
+      }
 
-        this.result.addEventListener('click', () => {
-            if (this.mathPanel.innerHTML.includes('+')) {
-                let panel: any = this.mathPanel.innerHTML
-                const previus =  panel.split('+')
-                const resultOperation: Number = parseInt(previus[0]) + parseInt(previus[1])
-                this.mathPanel.innerHTML = resultOperation
-            }
+      if (this.mathPanel.innerHTML.includes("x")) {
+        let panel: String = this.mathPanel.innerHTML;
+        const previus = panel.split("x");
+        const resultOperation: Number =
+          parseInt(previus[0]) * parseInt(previus[1]);
+        this.mathPanel.textContent = resultOperation.toString();
+      }
+    });
+  }
 
-            if (this.mathPanel.innerHTML.includes('-')) {
-                let panel: any = this.mathPanel.innerHTML
-                const previus =  panel.split('-')
-                const resultOperation: Number = parseInt(previus[0]) - parseInt(previus[1])
-                this.mathPanel.innerHTML = resultOperation
-            }
+  alterTheme(): void {
+    this.labels = document.querySelectorAll(".label-radio");
+    this.dataTheme = document.querySelectorAll("[data-theme]");
+    this.labels.forEach((label:Element) => {
+      label.addEventListener("click", () => {
+        this.labels.forEach((item:Element) =>{
+            item.classList.remove("selected");
+        });
+        label.classList.add("selected");
 
-            if (this.mathPanel.innerHTML.includes('/')) {
-                let panel: any = this.mathPanel.innerHTML
-                const previus =  panel.split('/')
-                const resultOperation: Number = parseInt(previus[0]) / parseInt(previus[1])
-                this.mathPanel.innerHTML = resultOperation
-            }
+        const corresponding = label.getAttribute("for");
 
-            if (this.mathPanel.innerHTML.includes('x')) {
-                let panel: any = this.mathPanel.innerHTML
-                const previus =  panel.split('x')
-                const resultOperation: Number = parseInt(previus[0]) * parseInt(previus[1])
-                this.mathPanel.innerHTML = resultOperation
-            }
-        })
-
-
-    }
-
-    alterTheme(): void {
-        this.labels = document.querySelectorAll('.label-radio')
-        this.dataTheme = document.querySelectorAll('[data-theme]')
-        this.labels.forEach(label => {
-
-            label.addEventListener('click', () => {
-                this.labels.forEach(item => {
-                    item.classList.remove('selected')
-                })
-                label.classList.add('selected')
-
-                const corresponding = label.getAttribute('for')
-
-                if (corresponding === 'radio-one') {
-                    this.dataTheme.forEach(data => {
-                        data.setAttribute('data-theme', 'one')
-                    })
-                } else if (corresponding == 'radio-two') {
-                    this.dataTheme.forEach(data => {
-                        data.setAttribute('data-theme', 'two')
-                    })
-                } else if (corresponding == 'radio-three') {
-                    this.dataTheme.forEach(data => {
-                        data.setAttribute('data-theme', 'three')
-                    })
-                }
-            })
-
-        })
-    }
+        if (corresponding === "radio-one") {
+          this.dataTheme.forEach((data:Element) => {
+            data.setAttribute("data-theme", "one");
+          });
+        } else if (corresponding == "radio-two") {
+          this.dataTheme.forEach((data:Element) => {
+            data.setAttribute("data-theme", "two");
+          });
+        } else if (corresponding == "radio-three") {
+          this.dataTheme.forEach((data:Element) => {
+            data.setAttribute("data-theme", "three");
+          });
+        }
+      });
+    });
+  }
 }
 
-
-
-
-
-export { Calculator }
+export { Calculator };
